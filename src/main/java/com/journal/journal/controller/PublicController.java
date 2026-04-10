0131a4993b5f2313f8bdd92febca813d76c56769
ayaz <ayaz.qureshi@hotelkeyapp.com>
2026-04-10 18:03:05 +0530

    fix in .xml file

--- src/main/java/com/journal/journal/controller/PublicController.java
+++ src/main/java/com/journal/journal/controller/PublicController.java
@@ -44,9 +44,9 @@ public class PublicController {
         try{
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
-        UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
-        String jwt = jwtUtil.generateToken(userDetails.getUsername());
-        return new ResponseEntity<>(jwt, HttpStatus.OK);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+                String jwt = jwtUtil.generateToken(userDetails.getUsername());
+                return new ResponseEntity<>(jwt, HttpStatus.OK);
         }catch (Exception e){
             return new ResponseEntity<>("Incorrect UserName or Password", HttpStatus.BAD_REQUEST);
         }
